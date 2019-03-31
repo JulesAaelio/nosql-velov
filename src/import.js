364e99f95c3bd5982e42c7e45408e9d1d2e6bf7d
Jules LAURENT <jules.laurent@ynov.com>
2019-03-31 20:28:49 +0200

    wip db connection

--- src/import.js
+++ src/import.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 console.log(`Import started at ${new Date()}`);
 
 const request = require('request-promise-native');
+const mongo = require('mongodb').MongoClient
 
 request.get('https://download.data.grandlyon.com/wfs/rdata?SERVICE=WFS&VERSION=2.0.0&outputformat=GEOJSON&maxfeatures=10000&request=GetFeature&typename=jcd_jcdecaux.jcdvelov&SRSNAME=urn:ogc:def:crs:EPSG::4171')
     .then(response => {
